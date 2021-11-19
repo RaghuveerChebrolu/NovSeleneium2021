9362245a135f7828d7a3e50e3f6b417f61ef1336
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-11-19 09:23:53 +0530

    19nov2021 selenium Handling Windows

--- src/test/java/com/testNg/TestCases/testNgTestCases2.java
+++ src/test/java/com/testNg/TestCases/testNgTestCases2.java
@@ -16,6 +16,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Alert;
@@ -51,7 +52,7 @@ public class testNgTestCases2 {
 		String GMOtitle = driver.getTitle();
 		System.out.println(GMOtitle);
 		Assert.assertEquals(GMOtitle, "Welcome to Green Mountain Outpost");
-		
+
 	}
 
 	@Test(priority = 1, dependsOnMethods = { "ValidateGMOOnLineLoadedSuccessfully" })
@@ -63,101 +64,131 @@ public class testNgTestCases2 {
 		driver.findElement(By.name("QTY_BACKPACKS")).sendKeys("4");
 		driver.findElement(By.name("QTY_BACKPACKS")).clear();
 		driver.findElement(By.name("QTY_BACKPACKS")).sendKeys("6");
-		//driver.close();
-		/*Thread.sleep(5000);
-		driver.navigate().back();
-		Thread.sleep(5000);
-		driver.navigate().forward();
-		driver.navigate().refresh();*/
+		// driver.close();
+		/*
+		 * Thread.sleep(5000); driver.navigate().back(); Thread.sleep(5000);
+		 * driver.navigate().forward(); driver.navigate().refresh();
+		 */
 		driver.findElement(By.name("bSubmit")).click();
 	}
-	
-	@Test(priority = 2,dependsOnMethods = { "ValidateEnterGmoOnLine" })
-	public void ValidatePlaceOrderPage(){
+
+	@Test(priority = 2, dependsOnMethods = { "ValidateEnterGmoOnLine" })
+	public void ValidatePlaceOrderPage() {
 		waitForPageToLoad();
 		String Title = driver.findElement(By.xpath("//h1[contains(text(),'Place Order')]")).getText();
-		System.out.println("Title: "+Title);
+		System.out.println("Title: " + Title);
 		Assert.assertEquals(Title, "Place Order");
 		String UnitPrice = driver.findElement(By.xpath("//table/tbody/tr[2]/td[4]")).getText();
 		System.out.println(UnitPrice);
 		String FloatValue = UnitPrice.substring(2).trim();
-		System.out.println("FloatValue: "+FloatValue);
-		//int UNiPriceintergervalue = Integer.parseInt(FloatValue);
-		//System.out.println(UNiPriceintergervalue);
-		float calculatedFloatValue = Float.parseFloat(FloatValue)*6;
+		System.out.println("FloatValue: " + FloatValue);
+		// int UNiPriceintergervalue = Integer.parseInt(FloatValue);
+		// System.out.println(UNiPriceintergervalue);
+		float calculatedFloatValue = Float.parseFloat(FloatValue) * 6;
 		System.out.println(calculatedFloatValue);
 		String TotalPrice = driver.findElement(By.xpath("//table/tbody/tr[2]/td[5]")).getText();
 		String TotalPricefromApplication = TotalPrice.substring(2).trim();
 		float FloatValuefromApplication = Float.parseFloat(TotalPricefromApplication);
-		System.out.println("FloatValuefromApplication: "+FloatValuefromApplication);
+		System.out.println("FloatValuefromApplication: " + FloatValuefromApplication);
 		Assert.assertEquals(calculatedFloatValue, FloatValuefromApplication);
 	}
-	
-	@Test(priority=3)
-	public void ValidatingAlerts(){
+
+	@Test(priority = 3)
+	public void ValidatingAlerts() {
 		System.out.println("inside ValidatingAlerts");
 		driver.navigate().to(ObjProperty.getProperty("AlertURL"));
 		waitForPageToLoad();
 		driver.findElement(By.id("alertButton")).click();
 		Alert ObjalertbuttonT = driver.switchTo().alert();
 		String alertbuttonText = ObjalertbuttonT.getText();
-		System.out.println("alertbuttonText: "+alertbuttonText);
+		System.out.println("alertbuttonText: " + alertbuttonText);
 		Assert.assertEquals(alertbuttonText, "You clicked a button");
 		ObjalertbuttonT.accept();
-		
+
 		driver.findElement(By.id("timerAlertButton")).click();
-		WebDriverWait wait = new WebDriverWait (driver,60);
+		WebDriverWait wait = new WebDriverWait(driver, 60);
 		wait.until(ExpectedConditions.alertIsPresent());
 		Alert ObjectTimerAlert = driver.switchTo().alert();
 		String TimerAlertText = ObjectTimerAlert.getText();
-		System.out.println("TimerAlertText: "+TimerAlertText);
-		//Assert.assertEquals(TimerAlertText, "This alert appeared after 5 second");
-		
+		System.out.println("TimerAlertText: " + TimerAlertText);
+		// Assert.assertEquals(TimerAlertText, "This alert appeared after 5
+		// second");
+
 		SoftAssert objSF = new SoftAssert();
 		objSF.assertEquals(TimerAlertText, "This alert appeared after 5 second");
 		ObjalertbuttonT.accept();
-		
+
 		driver.findElement(By.id("confirmButton")).click();
 		Alert ObjectconfirmButton = driver.switchTo().alert();
 		String confirmButtonAlertText = ObjectconfirmButton.getText();
-		System.out.println("confirmButtonAlertText: "+confirmButtonAlertText);
+		System.out.println("confirmButtonAlertText: " + confirmButtonAlertText);
 		Assert.assertEquals(confirmButtonAlertText, "Do you confirm action?");
 		ObjalertbuttonT.dismiss();
-		//span[@id='confirmResult']
+		// span[@id='confirmResult']
 		String confirmbuttontext = driver.findElement(By.xpath("//span[@id='confirmResult']")).getText();
 		Assert.assertEquals(confirmbuttontext, "You selected Cancel");
-		
+
 		driver.findElement(By.id("promtButton")).click();
-		Alert ObjectpromtButton= driver.switchTo().alert();
+		Alert ObjectpromtButton = driver.switchTo().alert();
 		ObjectpromtButton.sendKeys("How are you");
 		ObjalertbuttonT.accept();
 		String promptResultext = driver.findElement(By.id("promptResult")).getText();
 		Assert.assertEquals(promptResultext, "You entered How are you");
 		objSF.assertAll();
 	}
-	
-	@Test(priority=4)
-	public void ValditingHAndlingOfFrames(){
+
+	@Test(priority = 4)
+	public void ValditingHAndlingOfFrames() {
 		System.out.println("inside ValditingHAndlingOfFrames");
 		driver.navigate().to(ObjProperty.getProperty("FramesURL"));
 		waitForPageToLoad();
 		driver.switchTo().frame("singleframe");
-		
+
 		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("inside single frame");
-		
+
 		driver.switchTo().defaultContent();
 		driver.findElement(By.xpath("//a[contains(text(),'in an Iframe')]")).click();
-	
+
 		WebElement multipleframe = driver.findElement(By.xpath("//iframe[@src='MultipleFrames.html']"));
 		driver.switchTo().frame(multipleframe);
-		
+
 		WebElement singleframe = driver.findElement(By.xpath("//iframe[@src='SingleFrame.html']"));
 		driver.switchTo().frame(singleframe);
-		
+
 		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("single frame with in multi frame");
 		driver.switchTo().defaultContent();
 	}
-	
+
+	@Test(priority = 5)
+	public void HandlingOfWindows() {
+		System.out.println("inside HandlingOfWindows");
+		driver.navigate().to(ObjProperty.getProperty("WindowsURL"));
+		waitForPageToLoad();
+		String ParentWindow = driver.getWindowHandle();
+		Set<String> Allwindows = driver.getWindowHandles();
+		for (String Individualwindow : Allwindows) {
+			driver.switchTo().window(Individualwindow);
+			String title = driver.getTitle();
+			System.out.println(title);
+			if (title.equals("Tech Mahindra")) {
+				String url = driver.getCurrentUrl();
+				System.out.println("url:" + url);
+			}
+			else if (title.equals("ICICI")) {
+				driver.close();
+			} else if (title.contains("Job Search")) {
+				String pagesource = driver.getPageSource();
+				System.out.println("---------------");
+				//System.out.println(pagesource);
+				System.out.println("---------------");
+			} else if (title.contains("Tech")) {
+				driver.manage().window().maximize();
+			} 
+			
+		}
+		driver.switchTo().window(ParentWindow);
+	}
+
 	@BeforeMethod
 	public void beforeMethod() {
 		System.out.println("inside beforeMethod");
@@ -184,8 +215,6 @@ public class testNgTestCases2 {
 		launchBrowser();
 	}
 
-	
-
 	@AfterTest
 	public void afterTest() {
 		System.out.println("inside afterTest");
@@ -194,46 +223,46 @@ public class testNgTestCases2 {
 	@BeforeSuite
 	public void beforeSuite() {
 		System.out.println("inside beforeSuite");
-	/*	WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		
-		  WebDriverManager.edgedriver().setup();
-		  driver=new EdgeDriver();
-		 
-		driver.get("http://demo.borland.com/gmopost/");
-		driver.manage().window().maximize();
-		//note : implicit wait : This is a global waiting mechanism which is applicable
-		//for all web Elements through out the drive instance. It will wait until driver is able
-		//to find the locator type and it will process if it is able to identify the element locator 
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-*/
+		/*
+		 * WebDriverManager.chromedriver().setup(); driver = new ChromeDriver();
+		 * 
+		 * WebDriverManager.edgedriver().setup(); driver=new EdgeDriver();
+		 * 
+		 * driver.get("http://demo.borland.com/gmopost/");
+		 * driver.manage().window().maximize(); //note : implicit wait : This is
+		 * a global waiting mechanism which is applicable //for all web Elements
+		 * through out the drive instance. It will wait until driver is able
+		 * //to find the locator type and it will process if it is able to
+		 * identify the element locator
+		 * driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		 */
 		try {
 			readpropertyfile();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	@AfterSuite
 	public void afterSuite() {
 		System.out.println("inside afterSuite");
 	}
-	
-	//Helper Methods
+
+	// Helper Methods
 	private void readpropertyfile() throws IOException {
 		System.out.println(System.getProperty("user.dir"));
-		File Fileobj = new File(System.getProperty("user.dir")+"//src//test//resources//config.properties");
+		File Fileobj = new File(System.getProperty("user.dir") + "//src//test//resources//config.properties");
 		try {
 			FileInputStream Fileinput = new FileInputStream(Fileobj);
-			
+
 			ObjProperty.load(Fileinput);
 			ObjProperty.getProperty("browser");
 			ObjProperty.getProperty("GmoOnloneURL_SIT");
 			System.out.println(ObjProperty.getProperty("browser"));
 			System.out.println(ObjProperty.getProperty("GmoOnloneURL_SIT"));
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -242,10 +271,10 @@ public class testNgTestCases2 {
 
 	private void launchBrowser() {
 		String browser = ObjProperty.getProperty("browser");
-		switch(browser){
+		switch (browser) {
 		case "IE":
 			WebDriverManager.iedriver().setup();
-			driver = new InternetExplorerDriver(); 
+			driver = new InternetExplorerDriver();
 			break;
 		case "chrome":
 			WebDriverManager.chromedriver().setup();
@@ -263,17 +292,20 @@ public class testNgTestCases2 {
 			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
 		default:
-			//driver = new HtmlUnitDriver;
+			// driver = new HtmlUnitDriver;
 		}
 		driver.get(ObjProperty.getProperty("GmoOnloneURL_SIT"));
 		driver.manage().window().maximize();
-		//note : implicit wait : This is a global waiting mechanism which is applicable
-		//for all web Elements through out the drive instance. It will wait until driver is able
-		//to find the locator type and it will process if it is able to identify the element locator 
+		// note : implicit wait : This is a global waiting mechanism which is
+		// applicable
+		// for all web Elements through out the drive instance. It will wait
+		// until driver is able
+		// to find the locator type and it will process if it is able to
+		// identify the element locator
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
+
 	}
-	
+
 	public void waitForPageToLoad() {
 		ExpectedCondition<Boolean> pageLoadCondition = new ExpectedCondition<Boolean>() {
 			public Boolean apply(WebDriver driver) {
@@ -281,7 +313,7 @@ public class testNgTestCases2 {
 			}
 		};
 		// explicit wait -> Applicable for one webEllement
-		WebDriverWait wait = new WebDriverWait(driver, 60);//60 seconds 
+		WebDriverWait wait = new WebDriverWait(driver, 60);// 60 seconds
 		wait.until(pageLoadCondition);
 	}
 }
