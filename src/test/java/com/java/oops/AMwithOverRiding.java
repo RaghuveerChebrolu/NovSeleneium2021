a05991867e951dba1a22ae94b3ddbd3e204f9753
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2021-12-09 11:12:22 +0530

    09dec2021 java constructor, inheritance and super

---
+++ src/test/java/com/java/oops/AMwithOverRiding.java
@@ -0,0 +1,18 @@
+/*package com.java.OOPsConcepts;
+
+class A4545 {
+	protected void msg() {
+		System.out.println("Hello java");
+	}
+}
+
+public class AMwithOverRiding extends A4545 {
+	void msg() {
+		System.out.println("Hello java");// C.T.Error
+	}
+
+	public static void main(String args[]) {
+		AMwithOverRiding obj = new AMwithOverRiding();
+		obj.msg();
+	}
+}*/
